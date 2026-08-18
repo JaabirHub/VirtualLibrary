8bbebdc1311578fdc75cd240c70f1e749e738cc7
Ahmed Omar Jaabir (2024) ZNAC277 <jaabir.ahmedomar.2024@live.rhul.ac.uk>
2026-08-19 00:18:30 +0100

    Added service and controller classes for book packages. #3

--- backend/src/main/java/com/jaabir/backend/book/BookRepository.java
+++ backend/src/main/java/com/jaabir/backend/book/BookRepository.java
@@ -1,9 +1,10 @@
 package com.jaabir.backend.book;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, UUID>{
-  
+  Optional<Book> findByGoogleVolumeId(String googleVolumeId);
 }
